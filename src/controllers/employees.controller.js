8278e603d41038df64b2e72d1ed0903f3cb38ed0
javier bueno <javiworks01@gmail.com>
2023-12-20 16:27:49 +0100

    getEmployeeById

--- src/controllers/employees.controller.js
+++ src/controllers/employees.controller.js
@@ -3,14 +3,21 @@ import { pool } from '../db.js';
 export const getEmployees = async (req, res) => {
     
     const [rows] = await pool.query('SELECT * FROM employee');
-    res.status(200).send({success: true, data: rows || []});
+    res.status(200).json({success: true, data: rows || []});
 
 };
 
 
-export const getEmployeeById = (req, res) => {
+export const getEmployeeById = async (req, res) => {
+
+    const {id} = req.params;
+    const [rows] = await pool.query('SELECT * FROM employee WHERE id = ?', [id]);
     
-    res.status(200).send('Obteniendo empleado');
+    if (rows.length <= 0) {
+        return res.status(404).json({ success: false, message: 'Employee not found' , data: []});
+    }
+
+    res.status(200).json({success: true, data: rows[0] });
 
 };
 
@@ -33,7 +40,7 @@ export const createEmployee = async (req, res) => {
         [name, salary]
     );
      
-    res.status(201).send({success: true, data: { id: rows.insertId, name, salary}});
+    res.status(201).json({success: true, data: { id: rows.insertId, name, salary}});
 
 };
 
